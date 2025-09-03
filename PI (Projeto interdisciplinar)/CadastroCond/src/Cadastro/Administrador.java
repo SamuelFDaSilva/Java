72dec7796a0e4657b5402b0314d22bfefa75122f
Samuel Ferreira da Silva <fsmsamu928@gmail.com>
2025-09-03 08:21:13 -0300

    Add files via upload

---
+++ PI (Projeto interdisciplinar)/CadastroCond/src/Cadastro/Administrador.java
@@ -0,0 +1,14 @@
+package Cadastro;
+
+
+
+public class Administrador extends Usuario {
+
+    public Administrador(int idUsuario, String nomeUsuario, String senhaUsuario) {
+        super(idUsuario, nomeUsuario, senhaUsuario);
+    
+   
+}
+    
+}
+
---
+++ PI (Projeto interdisciplinar)/CadastroCond/src/Cadastro/PainelSolar.java
@@ -0,0 +1,24 @@
+package Cadastro;
+
+public class PainelSolar {
+		
+		private int idPlaca;
+		private double potencia;
+
+		public PainelSolar(int idPlaca, double potencia) {
+			this.idPlaca = idPlaca;
+			this.potencia = potencia;
+		}
+		
+		public int getidPlaca() {
+			return idPlaca;
+		}
+		
+		public double getPotencia() {
+			return potencia;
+		}
+		
+
+		
+	}
+
